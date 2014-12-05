4bfa4ec4fbafa5035561868efef26314957dc779
anilkuncham@gmail.com <anilkuncham@gmail.com>
2014-12-05 07:50:57 -0800

    priority queue

---
+++ PriorityQueuePatientTest.java
@@ -0,0 +1,40 @@
+package interview;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+public class PriorityQueuePatientTest {
+
+	public static final Comparator<Patient> pqComparator = new Comparator<Patient>(){
+
+		@Override
+		public int compare(Patient patient1, Patient patient2) {
+			return (patient1.isEmergencyCase() == patient2.isEmergencyCase()) ? (Integer.valueOf(patient1.getId()).compareTo(patient2.getId()))
+                    : (patient1.isEmergencyCase() ? -1 : 1);
+		}
+		
+	};
+
+public static void main(String[] args) {
+	
+	
+	
+    PriorityQueue<Patient> patientQueue = new PriorityQueue<Patient>(10, PriorityQueuePatientTest.pqComparator);
+    patientQueue.add(new Patient(1, "Patient1", false));
+    patientQueue.add(new Patient(2, "Patient2", false));
+    patientQueue.add(new Patient(3, "Patient3", true));
+    patientQueue.add(new Patient(4, "Patient4", false));
+    patientQueue.add(new Patient(5, "Patient5", true));
+
+    System.out.println();
+    System.out.print("Doctor's waiting for patients  : ");
+    while(true) {
+        Patient currentPatient = patientQueue.poll();
+        if(currentPatient == null) {
+            break;
+        }
+
+        System.out.print(currentPatient.getName() + " <-- ");
+    }
+    System.out.println();
+}
+}
